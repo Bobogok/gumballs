d2e92bd6fee92f8c5795d1e92d98924b35187606
Ivan M <vania.morskov@yandex.ru>
2021-04-02 20:28:37 +0300

    remore old code XML, add new script

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 window.onload = function() {
-    var url = 'http://localhost/gumballs/sales.json';
-    // var url = 'http://gumball.wickedlysmart.com/';
-    var request = new XMLHttpRequest();
-    // request.open('GET', url); lvl2 XML
-    request.onreadystatechange = function() { // lvl1 XML
-    // request.onload = function() { // lvl2 XML
-        if (request.readyState == 4 && request.status == 200) {
-            updateSales(request.responseText)
-            // console.log(request.responseText)
-        }
-    };
-    request.open('GET', url); // lvl 1 XML
-    request.send(null);
+    
 }
 
-function updateSales(responseText) {
+function updateSales(sales) {
+    //функция на данный момент возвращает строку
     var salesDiv = document.getElementById('sales');
-    var sales = JSON.parse(responseText); // преобразуем responseText из строки в JS эквивалент с помощью JSON.parse (массив)
     for (var i = 0; i < sales.length; i++) {
         var sale = sales[i];
         var div = document.createElement('div'); // для каждого элемента данных создаем div
