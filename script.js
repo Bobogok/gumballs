765f4fbbdcd0a77a39fe86f525483381a656c3fa
Ivan M <vania.morskov@yandex.ru>
2021-04-02 22:11:03 +0300

    add f handleRefresh, fix a few lines of code, delete tag script in html

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
-    
+    setInterval(handleRefresh, 3000); 
 }
+
+var lastReportTime = 0;
 
 function updateSales(sales) {
     //функция на данный момент возвращает строку
@@ -11,5 +13,28 @@ function updateSales(sales) {
         div.setAttribute('class', 'saleItem'); // присваиваем элементу класс saleitem
         div.innerHTML = sale.name + ' sold ' + sale.sales + ' gumballs'; // задаем содержимое для элемента div
         salesDiv.appendChild(div);
+    }
+    if (sales.length > 0) {
+        lastReportTime = sales[sales.length-1].time; //необходимо убедиться в НАЛИЧИИ массива; в случае
+        // отсутствия новых отчетов о продажах будет возвращен пустой массив и наш код сгене рирует исключение.
+    }
+}
+
+function handleRefresh() {
+    var url = 'http://gumball.wickedlysmart.com' +
+              '?callback=updateSales' +
+              '&lastreporttime=' + lastReportTime +
+              '&random=' + (new Date()).getTime(); //фиктивный параметр против кеширования
+    var newScriptElement = document.createElement('script');
+    newScriptElement.setAttribute('src', url);
+    newScriptElement.setAttribute('id', 'jsonp');
+
+    var oldScriptElement = document.getElementById('jsonp');
+    var head = document.getElementsByTagName('head')[0];
+    if (oldScriptElement == null) { // проверяем, есть ли элемент в head
+        head.appendChild(newScriptElement);
+    }
+    else {
+        head.replaceChild(newScriptElement, oldScriptElement); // заменяем элемент, если он уже был в head
     }
 }
